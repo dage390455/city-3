d06024ccb2562e4d5a332b4137437618e7c3ec0d
贺长寿 <hechangshou@sensoro.com>
2018-12-11 11:59:22 +0800

    初始配置界面搭建

---
+++ app/src/main/java/com/sensoro/smartcity/activity/DeployMonitorConfigurationActivity.java
@@ -0,0 +1,138 @@
+package com.sensoro.smartcity.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sensoro.smartcity.R;
+import com.sensoro.smartcity.base.BaseActivity;
+import com.sensoro.smartcity.imainviews.IDeployMonitorConfigurationView;
+import com.sensoro.smartcity.presenter.DeployMonitorConfigurationPresenter;
+import com.sensoro.smartcity.widget.toast.SensoroToast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class DeployMonitorConfigurationActivity extends BaseActivity<IDeployMonitorConfigurationView, DeployMonitorConfigurationPresenter>
+        implements IDeployMonitorConfigurationView {
+    @BindView(R.id.include_text_title_imv_arrows_left)
+    ImageView includeTextTitleImvArrowsLeft;
+    @BindView(R.id.include_text_title_tv_title)
+    TextView includeTextTitleTvTitle;
+    @BindView(R.id.include_text_title_tv_subtitle)
+    TextView includeTextTitleTvSubtitle;
+    @BindView(R.id.include_text_title_divider)
+    View includeTextTitleDivider;
+    @BindView(R.id.include_text_title_cl_root)
+    ConstraintLayout includeTextTitleClRoot;
+    @BindView(R.id.ac_deploy_configuration_et_enter)
+    EditText acDeployConfigurationEtEnter;
+    @BindView(R.id.ac_deploy_configuration_et_root)
+    LinearLayout acDeployConfigurationEtRoot;
+    @BindView(R.id.ac_deploy_configuration_tv_enter_tip)
+    TextView acDeployConfigurationTvEnterTip;
+    @BindView(R.id.ac_deploy_configuration_tv_configuration)
+    TextView acDeployConfigurationTvConfiguration;
+
+    @Override
+    protected void onCreateInit(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_deploy_configuration);
+        ButterKnife.bind(this);
+        initView();
+
+    }
+
+    private void initView() {
+        acDeployConfigurationEtEnter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().length()>0) {
+                    acDeployConfigurationTvConfiguration.setClickable(true);
+                    acDeployConfigurationTvConfiguration.setBackgroundResource(R.drawable.shape_bg_corner_29c_shadow);
+                }else{
+                    acDeployConfigurationTvConfiguration.setClickable(false);
+                    acDeployConfigurationTvConfiguration.setBackgroundResource(R.drawable.shape_bg_solid_df_corner);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected DeployMonitorConfigurationPresenter createPresenter() {
+        return new DeployMonitorConfigurationPresenter();
+    }
+
+    @Override
+    public void startAC(Intent intent) {
+        startActivity(intent);
+    }
+
+    @Override
+    public void finishAc() {
+        finish();
+    }
+
+    @Override
+    public void startACForResult(Intent intent, int requestCode) {
+
+    }
+
+    @Override
+    public void setIntentResult(int resultCode) {
+
+    }
+
+    @Override
+    public void setIntentResult(int resultCode, Intent data) {
+
+    }
+
+    @Override
+    public void showProgressDialog() {
+
+    }
+
+    @Override
+    public void dismissProgressDialog() {
+
+    }
+
+    @Override
+    public void toastShort(String msg) {
+        SensoroToast.INSTANCE.makeText(msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void toastLong(String msg) {
+
+    }
+
+
+    @OnClick({R.id.ac_deploy_configuration_tv_enter_tip, R.id.ac_deploy_configuration_tv_configuration})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.ac_deploy_configuration_tv_configuration:
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/sensoro/smartcity/presenter/DeployMonitorConfigurationPresenter.java
@@ -0,0 +1,19 @@
+package com.sensoro.smartcity.presenter;
+
+import android.content.Context;
+
+import com.sensoro.smartcity.base.BaseActivity;
+import com.sensoro.smartcity.base.BasePresenter;
+import com.sensoro.smartcity.imainviews.IDeployMonitorConfigurationView;
+
+public class DeployMonitorConfigurationPresenter extends BasePresenter<IDeployMonitorConfigurationView> {
+    @Override
+    public void initData(Context context) {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+}
