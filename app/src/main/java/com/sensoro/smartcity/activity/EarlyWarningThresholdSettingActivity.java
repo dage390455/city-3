b4b52e37435f28d733413fc4562b2ec7e6b8461f
dongdong <dongdong@sensoro.com>
2018-12-26 14:25:46 +0800

    v

--- app/src/main/java/com/sensoro/smartcity/activity/EarlyWarningThresholdSettingActivity.java
+++ app/src/main/java/com/sensoro/smartcity/activity/EarlyWarningThresholdSettingActivity.java
@@ -1,30 +1,15 @@
 package com.sensoro.smartcity.activity;
 
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.view.KeyEvent;
-import android.view.View;
-import android.webkit.JavascriptInterface;
-import android.webkit.JsResult;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import com.sensoro.smartcity.R;
 import com.sensoro.smartcity.base.BaseActivity;
 import com.sensoro.smartcity.imainviews.IEarlyWarningThresholdSettingActivityView;
 import com.sensoro.smartcity.presenter.EarlyWarningThresholdSettingActivityPresenter;
-import com.sensoro.smartcity.util.LogUtils;
 
 import butterknife.ButterKnife;
 
 public class EarlyWarningThresholdSettingActivity extends BaseActivity<IEarlyWarningThresholdSettingActivityView, EarlyWarningThresholdSettingActivityPresenter> implements IEarlyWarningThresholdSettingActivityView {
-    private ProgressBar progressBar;
-    private WebView webView;
 
     @Override
     protected void onCreateInit(Bundle savedInstanceState) {
@@ -35,124 +20,7 @@ public class EarlyWarningThresholdSettingActivity extends BaseActivity<IEarlyWar
     }
 
     private void iniView() {
-        progressBar = (ProgressBar) findViewById(R.id.progressbar);//进度条
 
-        webView = (WebView) findViewById(R.id.webview);
-//        webView.loadUrl("file:///android_asset/test.html");//加载asset文件夹下html
-        //
-        String url = "https://testapi.fadada.com:8443/api//viewdocs.action?app_id=401676&timestamp=20181225120216&v=2.0&msg_digest=RTM0QTc2QUVCNTlDMDZBM0I3QTQwQzIwRTYxQjhGOTU0QkEyMDAyMA==&send_app_id=null&transaction_id=1111";
-//        webView.loadUrl("http://139.196.35.30:8080/OkHttpTest/apppackage/test.html");//加载url
-        webView.loadUrl(url);//加载url
-
-        //使用webview显示html代码
-//        webView.loadDataWithBaseURL(null,"<html><head><title> 欢迎您 </title></head>" +
-//                "<body><h2>使用webview显示 html代码</h2></body></html>", "text/html" , "utf-8", null);
-
-        webView.addJavascriptInterface(this, "android");//添加js监听 这样html就能调用客户端
-        webView.setWebChromeClient(webChromeClient);
-        webView.setWebViewClient(webViewClient);
-
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);//允许使用js
-
-        /**
-         * LOAD_CACHE_ONLY: 不使用网络，只读取本地缓存数据
-         * LOAD_DEFAULT: （默认）根据cache-control决定是否从网络上取数据。
-         * LOAD_NO_CACHE: 不使用缓存，只从网络获取数据.
-         * LOAD_CACHE_ELSE_NETWORK，只要本地有，无论是否过期，或者no-cache，都使用缓存中的数据。
-         */
-        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);//不使用缓存，只从网络获取数据.
-
-        //支持屏幕缩放
-        webSettings.setSupportZoom(true);
-        webSettings.setBuiltInZoomControls(true);
-
-        //不显示webview缩放按钮
-//        webSettings.setDisplayZoomControls(false);
-    }
-
-    //WebViewClient主要帮助WebView处理各种通知、请求事件
-    private WebViewClient webViewClient = new WebViewClient() {
-        @Override
-        public void onPageFinished(WebView view, String url) {//页面加载完成
-            progressBar.setVisibility(View.GONE);
-        }
-
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {//页面开始加载
-            progressBar.setVisibility(View.VISIBLE);
-        }
-
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            LogUtils.loge("ddong", "拦截url:" + url);
-            if (url.equals("http://www.google.com/")) {
-                Toast.makeText(mActivity, "国内不能访问google,拦截该url", Toast.LENGTH_LONG).show();
-                return true;//表示我已经处理过了
-            }
-            return super.shouldOverrideUrlLoading(view, url);
-        }
-
-    };
-    //WebChromeClient主要辅助WebView处理Javascript的对话框、网站图标、网站title、加载进度等
-    private WebChromeClient webChromeClient = new WebChromeClient() {
-        //不支持js的alert弹窗，需要自己监听然后通过dialog弹窗
-        @Override
-        public boolean onJsAlert(WebView webView, String url, String message, JsResult result) {
-            AlertDialog.Builder localBuilder = new AlertDialog.Builder(webView.getContext());
-            localBuilder.setMessage(message).setPositiveButton("确定", null);
-            localBuilder.setCancelable(false);
-            localBuilder.create().show();
-
-            //注意:
-            //必须要这一句代码:result.confirm()表示:
-            //处理结果为确定状态同时唤醒WebCore线程
-            //否则不能继续点击按钮
-            result.confirm();
-            return true;
-        }
-
-        //获取网页标题
-        @Override
-        public void onReceivedTitle(WebView view, String title) {
-            super.onReceivedTitle(view, title);
-            LogUtils.loge("ddong", "网页标题:" + title);
-        }
-
-        //加载进度回调
-        @Override
-        public void onProgressChanged(WebView view, int newProgress) {
-            progressBar.setProgress(newProgress);
-        }
-    };
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        LogUtils.loge("ddong", "是否有上一个页面:" + webView.canGoBack());
-        if (webView.canGoBack() && keyCode == KeyEvent.KEYCODE_BACK) {//点击返回按钮的时候判断有没有上一页
-            webView.goBack(); // goBack()表示返回webView的上一页面
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-
-    /**
-     * JS调用android的方法
-     *
-     * @param str
-     * @return
-     */
-    @JavascriptInterface //仍然必不可少
-    public void getClient(String str) {
-        LogUtils.loge("ddong", "html调用客户端:" + str);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //释放资源
-        webView.destroy();
-        webView = null;
     }
 
     @Override
