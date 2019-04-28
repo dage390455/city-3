677f88eabdfbe5e810b6c68066292b296c306655
贺长寿 <hechangshou@sensoro.com>
2019-04-28 15:53:54 +0800

    摄像头模块初步完成

--- app/src/main/java/com/sensoro/smartcity/activity/CameraDetailActivity.java
+++ app/src/main/java/com/sensoro/smartcity/activity/CameraDetailActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
---
+++ app/src/main/java/com/sensoro/smartcity/adapter/PersonLocusCameraGaoDeAdapter.java
@@ -0,0 +1,262 @@
+package com.sensoro.smartcity.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.model.Marker;
+import com.sensoro.smartcity.R;
+import com.sensoro.smartcity.activity.CameraDetailActivity;
+import com.sensoro.smartcity.activity.PersonLocusActivity;
+import com.sensoro.smartcity.base.BaseActivity;
+import com.sensoro.smartcity.widget.CustomStandardGSYVideoPlayer;
+import com.shuyu.gsyvideoplayer.GSYVideoManager;
+import com.shuyu.gsyvideoplayer.builder.GSYVideoOptionBuilder;
+import com.shuyu.gsyvideoplayer.listener.GSYSampleCallBack;
+import com.shuyu.gsyvideoplayer.listener.LockClickListener;
+import com.shuyu.gsyvideoplayer.utils.NetworkUtils;
+import com.shuyu.gsyvideoplayer.utils.OrientationUtils;
+import com.shuyu.gsyvideoplayer.video.base.GSYVideoPlayer;
+
+public class PersonLocusCameraGaoDeAdapter implements AMap.InfoWindowAdapter {
+    private final BaseActivity mContext;
+    private CustomStandardGSYVideoPlayer player;
+    private OrientationUtils orientationUtils;
+    private GSYVideoOptionBuilder gsyVideoOption;
+    private boolean isPlay;
+    private boolean isPause;
+    private final View view;
+
+    public PersonLocusCameraGaoDeAdapter(BaseActivity context) {
+        mContext = context;
+        view = LayoutInflater.from(mContext).inflate(R.layout.adapter_gao_de_info_window_person_locus, null);
+        player = view.findViewById(R.id.detail_player_adapter_gao_de_person_locus);
+
+        initPlayer();
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return view;
+    }
+
+    private void initPlayer() {
+        //外部辅助的旋转，帮助全屏
+        orientationUtils = new OrientationUtils(mContext, player);
+        //初始化不打开外部的旋转
+        orientationUtils.setEnable(false);
+        getCurPlay().getTitleTextView().setVisibility(View.VISIBLE);
+        //设置返回键
+        getCurPlay().getBackButton().setVisibility(View.VISIBLE);
+
+        getCurPlay().setEnlargeImageRes(R.drawable.ic_camera_full_screen);
+
+        getCurPlay().setShrinkImageRes(R.drawable.ic_camera_full_screen);
+
+        getCurPlay().getFullscreenButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //直接横屏
+                orientationUtils.resolveByClick();
+                //第一个true是否需要隐藏actionbar，第二个true是否需要隐藏statusbar
+                getCurPlay().startWindowFullscreen(mContext, true, true);
+            }
+        });
+
+        initVideoOption(null);
+
+//        gsyVideoOption = new GSYVideoOptionBuilder();
+//        gsyVideoOption
+////                .setThumbImageView(imageView)
+//                .setIsTouchWiget(true)
+//                .setRotateViewAuto(false)
+//                .setLockLand(false)
+//                .setAutoFullWithSize(false)
+//                .setShowFullAnimation(false)
+//                .setNeedLockFull(true)
+////                .setUrl(url)
+//                .setCacheWithPlay(false)
+//                .setVideoTitle("测试视频")
+//                .setVideoAllCallBack(new GSYSampleCallBack() {
+//                    @Override
+//                    public void onPrepared(String url, Object... objects) {
+//                        super.onPrepared(url, objects);
+//                        //开始播放了才能旋转和全屏
+//                        orientationUtils.setEnable(true);
+//                        isPlay = true;
+//                    }
+//
+//                    @Override
+//                    public void onQuitFullscreen(String url, Object... objects) {
+//                        super.onQuitFullscreen(url, objects);
+//                        if (orientationUtils != null) {
+//                            orientationUtils.backToProtVideo();
+//                        }
+//                    }
+//                }).setLockClickListener(new LockClickListener() {
+//            @Override
+//            public void onClick(View view, boolean lock) {
+//                if (orientationUtils != null) {
+//                    //配合下方的onConfigurationChanged
+//                    orientationUtils.setEnable(!lock);
+//                }
+//            }
+//        }).build(getCurPlay());
+////增加title
+//        getCurPlay().getTitleTextView().setVisibility(View.VISIBLE);
+//        //设置返回键
+//        getCurPlay().getBackButton().setVisibility(View.VISIBLE);
+//
+//        getCurPlay().setEnlargeImageRes(R.drawable.ic_camera_full_screen);
+//
+//        getCurPlay().setShrinkImageRes(R.drawable.ic_camera_full_screen);
+//
+//        getCurPlay().getFullscreenButton().setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //直接横屏
+//                orientationUtils.resolveByClick();
+//                //第一个true是否需要隐藏actionbar，第二个true是否需要隐藏statusbar
+//                getCurPlay().startWindowFullscreen(mContext, true, true);
+//            }
+//        });
+//        getCurPlay().getBackButton().setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mContext.finish();
+//            }
+//        });
+    }
+
+    public void initVideoOption(String url) {
+        player.changeBottomContainer(View.INVISIBLE);
+
+        //增加封面
+//        if (imageView == null) {
+//            imageView = new ImageView(this);
+//            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//            imageView.setImageResource(R.mipmap.ic_launcher);
+//        }
+
+
+        gsyVideoOption = new GSYVideoOptionBuilder();
+        gsyVideoOption
+//                .setThumbImageView(imageView)
+                .setIsTouchWiget(true)
+                .setRotateViewAuto(false)
+                .setLockLand(false)
+                .setAutoFullWithSize(false)
+                .setShowFullAnimation(false)
+                .setNeedLockFull(true)
+//                .setUrl(url)
+                .setCacheWithPlay(false)
+                .setVideoTitle("测试视频")
+                .setVideoAllCallBack(new GSYSampleCallBack() {
+                    @Override
+                    public void onPrepared(String url, Object... objects) {
+                        super.onPrepared(url, objects);
+                        //开始播放了才能旋转和全屏
+                        orientationUtils.setEnable(true);
+                        isPlay = true;
+                    }
+
+                    @Override
+                    public void onQuitFullscreen(String url, Object... objects) {
+                        super.onQuitFullscreen(url, objects);
+                        if (orientationUtils != null) {
+                            orientationUtils.backToProtVideo();
+                        }
+                    }
+                }).setLockClickListener(new LockClickListener() {
+            @Override
+            public void onClick(View view, boolean lock) {
+                if (orientationUtils != null) {
+                    //配合下方的onConfigurationChanged
+                    orientationUtils.setEnable(!lock);
+                }
+            }
+        }).build(getCurPlay());
+        getCurPlay().getFullscreenButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //直接横屏
+                orientationUtils.resolveByClick();
+
+                //第一个true是否需要隐藏actionbar，第二个true是否需要隐藏statusbar
+                getCurPlay().startWindowFullscreen(mContext, true, true);
+            }
+        });
+        getCurPlay().getBackButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mContext.onBackPressed();
+            }
+        });
+//        getCurPlay().startPlayLogic();
+    }
+
+    private GSYVideoPlayer getCurPlay() {
+        if (player.getFullWindowPlayer() != null) {
+            return player.getFullWindowPlayer();
+        }
+        return player;
+    }
+
+    public void startPlayLogic(final String url1) {
+        if (!NetworkUtils.isAvailable(mContext)) {
+            orientationUtils.setEnable(false);
+            return;
+        }
+
+        if (!NetworkUtils.isWifiConnected(mContext)) {
+            orientationUtils.setEnable(false);
+            player.getPlayBtn().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    gsyVideoOption.setUrl(url1).build(getCurPlay());
+                    getCurPlay().startPlayLogic();
+
+                }
+            });
+            return;
+        }
+        player.changeBottomContainer(View.VISIBLE);
+
+        gsyVideoOption.setUrl(url1).build(getCurPlay());
+        getCurPlay().startPlayLogic();
+        orientationUtils.setEnable(true);
+
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+
+    public void destroyGsyVideo() {
+        if (isPlay) {
+            getCurPlay().release();
+        }
+        GSYVideoManager.releaseAllVideos();
+    }
+
+    public void onConfigurationChanged(Configuration newConfig) {
+        if (isPlay && !isPause) {
+            getCurPlay().onConfigurationChanged(mContext, newConfig, orientationUtils, true, true);
+        }
+    }
+
+    public void onPause() {
+        getCurPlay().onVideoPause();
+        isPause = true;
+    }
+
+    public void onResume() {
+//        getCurPlay().onVideoResume(false);
+        isPause = false;
+    }
+}
