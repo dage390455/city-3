46e18e56caa4f783e37ba6adcf506a4ca9f9c2d9
wlp <wlpgdd0725>
2019-05-16 20:59:19 +0800

    基站详情UI渐变动画

--- app/src/main/java/com/sensoro/smartcity/activity/BaseStationDetailActivity.java
+++ app/src/main/java/com/sensoro/smartcity/activity/BaseStationDetailActivity.java
@@ -6,6 +6,8 @@ import android.support.constraint.ConstraintLayout;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -364,9 +366,13 @@ public class BaseStationDetailActivity extends BaseActivity<IBaseStationDetailAc
 //
 //                    set2.setDrawVerticalHighlightIndicator(true);
 //                }
-                if (topStateRl.getVisibility() == View.GONE) {
-                    topStateRl.setVisibility(View.VISIBLE);
-                }
+//                if (topStateRl.getVisibility() == View.GONE) {
+//                    topStateRl.setVisibility(View.VISIBLE);
+//
+//                }
+
+
+                fadeIn(topStateRl);
 
             }
 
@@ -387,9 +393,12 @@ public class BaseStationDetailActivity extends BaseActivity<IBaseStationDetailAc
 //
 //                }
 
-                if (topStateRl.getVisibility() == View.VISIBLE) {
-                    topStateRl.setVisibility(View.GONE);
-                }
+
+                fadeOut(topStateRl);
+
+//                if (topStateRl.getVisibility() == View.VISIBLE) {
+//                    topStateRl.setVisibility(View.GONE);
+//                }
             }
 
             @Override
@@ -640,6 +649,34 @@ public class BaseStationDetailActivity extends BaseActivity<IBaseStationDetailAc
     @Override
     public void onNothingSelected() {
 
+    }
+
+    public static void fadeIn(View view, float startAlpha, float endAlpha, long duration) {
+        if (view.getVisibility() == View.VISIBLE) return;
+
+        view.setVisibility(View.VISIBLE);
+        Animation animation = new AlphaAnimation(startAlpha, endAlpha);
+        animation.setDuration(duration);
+        view.startAnimation(animation);
+    }
+
+    public static void fadeIn(View view) {
+        fadeIn(view, 0F, 1F, 400);
+
+        // We disabled the button in fadeOut(), so enable it here.
+        view.setEnabled(true);
+    }
+
+    public static void fadeOut(View view) {
+        if (view.getVisibility() != View.VISIBLE) return;
+
+        // Since the button is still clickable before fade-out animation
+        // ends, we disable the button first to block click.
+        view.setEnabled(false);
+        Animation animation = new AlphaAnimation(1F, 0F);
+        animation.setDuration(400);
+        view.startAnimation(animation);
+        view.setVisibility(View.GONE);
     }
 
 
