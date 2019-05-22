5985cc4fc924167081e6491372be878f03b8924c
wlp <wlpgdd0725>
2019-05-22 12:20:58 +0800

    录制小视频方案替换

--- app/src/main/java/com/sensoro/smartcity/imainviews/ITakeRecordActivityView.java
+++ app/src/main/java/com/sensoro/smartcity/imainviews/ITakeRecordActivityView.java
@@ -1,41 +1,13 @@
 package com.sensoro.smartcity.imainviews;
 
 import android.content.Intent;
-import android.view.SurfaceHolder;
 
 import com.sensoro.common.iwidget.IActivityIntent;
 import com.sensoro.common.iwidget.IProgressDialog;
 import com.sensoro.common.iwidget.IToast;
-import com.yixia.camera.MediaRecorderNative;
 
 public interface ITakeRecordActivityView extends IToast, IProgressDialog, IActivityIntent {
-    void showProgressDialog(int progress);
-
-    void dismissProgress();
-
-    void previewVideo(String videoPath);
-
-    SurfaceHolder getFfmpegHolder();
-
-    void startAnim();
-
-    void pausePlayVideo();
 
     void setFinishResult(int resultCodeRecord, Intent intent);
 
-    void recordFinish();
-
-    void updateStatusProgress(int duration);
-
-    void setFfmpegTouchFocus(MediaRecorderNative mMediaRecorder);
-
-    void rbStartCloseButton();
-
-    void setPlayLooping(boolean isLooping);
-
-    void startPlay();
-
-    void setStartRecordStatus();
-
-    void setRetakeStatus();
 }
