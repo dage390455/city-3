6162422e52f8731c5b98dd429d8f0bf5996e5c5d
dongdong <dongdong@sensoro.com>
2019-06-03 18:50:52 +0800

    加入nameplate组件化支持

--- nameplate/src/main/java/com/sensoro/nameplate/debug/NameplateApp.java
+++ nameplate/src/main/java/com/sensoro/nameplate/debug/NameplateApp.java
@@ -1,9 +1,11 @@
 package com.sensoro.nameplate.debug;
 
 import com.sensoro.common.base.BaseApplication;
+import com.sensoro.common.helper.PreferencesHelper;
 import com.sensoro.common.server.CityObserver;
 import com.sensoro.common.server.RetrofitServiceHelper;
 import com.sensoro.common.server.response.LoginRsp;
+import com.sensoro.common.utils.LogUtils;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
@@ -16,6 +18,10 @@ public class NameplateApp extends BaseApplication {
     }
 
     private void login() {
+        PreferencesHelper.getInstance().getSessionId();
+        PreferencesHelper.getInstance().saveMyBaseUrl("city-dev-api.sensoro.com/");
+        RetrofitServiceHelper.getInstance().saveBaseUrlType(5);
+        RetrofitServiceHelper.getInstance().getBaseUrlType();
         RetrofitServiceHelper.getInstance().login("15110041945", "aa1111", "").subscribeOn
                 (Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new CityObserver<LoginRsp>(null) {
             @Override
@@ -34,6 +40,11 @@ public class NameplateApp extends BaseApplication {
 
             @Override
             public void onErrorMsg(int errorCode, String errorMsg) {
+                try {
+                    LogUtils.loge(errorMsg);
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
 //                getView().dismissProgressDialog();
 //                getView().toastShort(errorMsg);
             }
