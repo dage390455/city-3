3a967e0cf57cc028d03e6482a314bfe9d96b3b9c
dongdong <dongdong@sensoro.com>
2019-06-05 14:18:42 +0800

    添加部署接口

--- lib_common/src/main/java/com/sensoro/common/constant/ARouterConstants.java
+++ lib_common/src/main/java/com/sensoro/common/constant/ARouterConstants.java
@@ -1,5 +1,6 @@
 package com.sensoro.common.constant;
 
 public interface ARouterConstants {
+    String AROUTER_PATH="arouter_path";
     String activity_deploy_detail = "/nameplate/activity/NameplateListActivity";
 }
