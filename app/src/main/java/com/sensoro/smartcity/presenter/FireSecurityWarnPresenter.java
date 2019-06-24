9cf1b4e86b83270ae3245c239f1eedd04573b886
bin.tian <bin.tian@sensoro.com>
2019-06-24 20:34:39 +0800

    集成安防预警列表页

--- app/src/main/java/com/sensoro/smartcity/presenter/FireSecurityWarnPresenter.java
+++ app/src/main/java/com/sensoro/smartcity/presenter/FireSecurityWarnPresenter.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.sensoro.city_camera.fragment.CameraListFragment;
+import com.sensoro.city_camera.fragment.CameraWarnListFragment;
 import com.sensoro.common.base.BasePresenter;
 import com.sensoro.common.constant.ARouterConstants;
 import com.sensoro.smartcity.fragment.FireWarnFragment;
@@ -20,7 +21,7 @@ import java.util.ArrayList;
 public class FireSecurityWarnPresenter extends BasePresenter<IFireSecurityWarnView> {
 
     private FireWarnFragment mFireWarnFragment;
-    private CameraListFragment mCameraListFragment;
+    private CameraWarnListFragment mCameraWarnListFragment;
 
     private final ArrayList<Fragment> mFragmentList = new ArrayList<>(2);
     private final ArrayList<String> mFragmentTitleList = new ArrayList<>(2);
@@ -32,11 +33,11 @@ public class FireSecurityWarnPresenter extends BasePresenter<IFireSecurityWarnVi
 
     private void initViewPager(Context context){
         mFireWarnFragment = new FireWarnFragment();
-        mCameraListFragment = (CameraListFragment) ARouter.getInstance().build(ARouterConstants.FRAGMENT_CAMERA_LIST).navigation();
+        mCameraWarnListFragment = new CameraWarnListFragment();
 
         mFragmentList.clear();
         mFragmentList.add(mFireWarnFragment);
-        mFragmentList.add(mCameraListFragment);
+        mFragmentList.add(mCameraWarnListFragment);
 
         mFragmentTitleList.add(context.getString(com.sensoro.common.R.string.fire_warn_title));
         mFragmentTitleList.add(context.getString(com.sensoro.common.R.string.security_warn_title));
