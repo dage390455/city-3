d0ae4f107d1391c4738712e7aa3187782278d09f
bin.tian <bin.tian@sensoro.com>
2019-06-26 15:29:33 +0800

    增加常量表

---
+++ city_camera/src/main/java/com/sensoro/city_camera/constants/SecurityConstants.java
@@ -0,0 +1,16 @@
+package com.sensoro.city_camera.constants;
+
+/**
+ * @author : bin.tian
+ * date   : 2019-06-26
+ */
+public interface SecurityConstants {
+    /**
+     * 预警有效
+     */
+    int SECURITY_VALID = 0;
+    /**
+     * 预警无效
+     */
+    int SECURITY_INVALID = 1;
+}
