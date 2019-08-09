ad4446196b39a2d81971d681d5ee87618cd0ff42
wlp <wlpgdd0725>
2019-08-09 18:49:00 +0800

    视频横屏弹窗显示不全

--- gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/utils/ChangeVideoFormatDialog.java
+++ gsyVideoPlayer-java/src/main/java/com/shuyu/gsyvideoplayer/utils/ChangeVideoFormatDialog.java
@@ -2,6 +2,7 @@ package com.shuyu.gsyvideoplayer.utils;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.os.Build;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,6 +29,8 @@ public class ChangeVideoFormatDialog {
         //自定义dialog显示风格
         dialog = new Dialog(context, R.style.change_video_formate_dialog_style);
         dialog.setCanceledOnTouchOutside(true);
+        dialog.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
         dialog.setContentView(inflate);
         Window window = dialog.getWindow();
         WindowManager.LayoutParams wlp = window.getAttributes();
@@ -37,6 +40,25 @@ public class ChangeVideoFormatDialog {
         window.setAttributes(wlp);
 
 
+        dialog.getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        //布局位于状态栏下方
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        //全屏
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        //隐藏导航栏
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+                if (Build.VERSION.SDK_INT >= 19) {
+                    uiOptions |= 0x00001000;
+                } else {
+                    uiOptions |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
+                }
+                dialog.getWindow().getDecorView().setSystemUiVisibility(uiOptions);
+            }
+        });
         flvformatTv.setOnClickListener(v -> {
 
             if (null != listener) {
