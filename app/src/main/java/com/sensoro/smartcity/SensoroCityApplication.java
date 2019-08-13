bf350e51a7f5500f3ce518f99ae774dda6a58d05
dongdong <dongdong@sensoro.com>
2019-08-13 16:02:28 +0800

    bugly升级优化

--- app/src/main/java/com/sensoro/smartcity/SensoroCityApplication.java
+++ app/src/main/java/com/sensoro/smartcity/SensoroCityApplication.java
@@ -360,142 +360,145 @@ public class SensoroCityApplication extends BaseApplication implements SensoroPu
     }
 
     private void initBugLy() {
-        try {
+        synchronized (SensoroCityApplication.class) {
+            try {
 //  在这里设置strategy的属性，在bugly初始化时传入
-            final CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(getApplicationContext());
-            strategy.setAppChannel("master");  //设置渠道
-            strategy.setAppVersion(BuildConfig.VERSION_NAME);      //App的版本
-            strategy.setAppPackageName("com.sensoro.smartcity");  //App的包名
-            //
-            /**
-             * true表示app启动自动初始化升级模块;
-             * false不会自动初始化;
-             * 开发者如果担心sdk初始化影响app启动速度，可以设置为false，
-             * 在后面某个时刻手动调用Beta.init(getApplicationContext(),false);
-             */
-            Beta.autoInit = false;
+                final CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(getApplicationContext());
+                strategy.setAppChannel("master");  //设置渠道
+                strategy.setAppVersion(BuildConfig.VERSION_NAME);      //App的版本
+                strategy.setAppPackageName("com.sensoro.smartcity");  //App的包名
+                //
+                /**
+                 * true表示app启动自动初始化升级模块;
+                 * false不会自动初始化;
+                 * 开发者如果担心sdk初始化影响app启动速度，可以设置为false，
+                 * 在后面某个时刻手动调用Beta.init(getApplicationContext(),false);
+                 */
+                Beta.autoInit = false;
 
-            /**
-             * true表示初始化时自动检查升级;
-             * false表示不会自动检查升级,需要手动调用Beta.checkUpgrade()方法;
-             */
-            Beta.autoCheckUpgrade = true;
+                /**
+                 * true表示初始化时自动检查升级;
+                 * false表示不会自动检查升级,需要手动调用Beta.checkUpgrade()方法;
+                 */
+                Beta.autoCheckUpgrade = true;
 
-            /**
-             * 设置升级检查周期为60s(默认检查周期为0s)，60s内SDK不重复向后台请求策略);
-             */
-            Beta.upgradeCheckPeriod = 60 * 1000;
+                /**
+                 * 设置升级检查周期为60s(默认检查周期为0s)，60s内SDK不重复向后台请求策略);
+                 */
+                Beta.upgradeCheckPeriod = 60 * 1000;
 
-            /**
-             * 设置启动延时为1s（默认延时3s），APP启动1s后初始化SDK，避免影响APP启动速度;
-             */
-            Beta.initDelay = 6 * 1000;
+                /**
+                 * 设置启动延时为1s（默认延时3s），APP启动1s后初始化SDK，避免影响APP启动速度;
+                 */
+                Beta.initDelay = 6 * 1000;
 
-            /**
-             * 设置通知栏大图标，largeIconId为项目中的图片资源;
-             */
+                /**
+                 * 设置通知栏大图标，largeIconId为项目中的图片资源;
+                 */
 //        Beta.largeIconId = R.drawable.ic_launcher;
 
-            /**
-             * 设置状态栏小图标，smallIconId为项目中的图片资源Id;
-             */
+                /**
+                 * 设置状态栏小图标，smallIconId为项目中的图片资源Id;
+                 */
 //        Beta.smallIconId = R.drawable.ic_launcher;
 
-            /**
-             * 设置更新弹窗默认展示的banner，defaultBannerId为项目中的图片资源Id;
-             * 当后台配置的banner拉取失败时显示此banner，默认不设置则展示“loading“;
-             */
+                /**
+                 * 设置更新弹窗默认展示的banner，defaultBannerId为项目中的图片资源Id;
+                 * 当后台配置的banner拉取失败时显示此banner，默认不设置则展示“loading“;
+                 */
 //        Beta.defaultBannerId = R.drawable.ic_launcher;
 
-            /**
-             * 设置sd卡的Download为更新资源保存目录;
-             * 后续更新资源会保存在此目录，需要在manifest中添加WRITE_EXTERNAL_STORAGE权限;
-             */
-            Beta.storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                /**
+                 * 设置sd卡的Download为更新资源保存目录;
+                 * 后续更新资源会保存在此目录，需要在manifest中添加WRITE_EXTERNAL_STORAGE权限;
+                 */
+                Beta.storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
 
-            /**
-             * 点击过确认的弹窗在APP下次启动自动检查更新时会再次显示;
-             */
-            Beta.showInterruptedStrategy = true;
-            //
-            Beta.upgradeDialogLayoutId = R.layout.layout_upgrade_dialog;
-            /**
-             * 只允许在MainActivity上显示更新弹窗，其他activity上不显示弹窗;
-             * 不设置会默认所有activity都可以显示弹窗;
-             */
-            Beta.canNotShowUpgradeActs.add(SplashActivity.class);
-            //设置是否显示消息通知
-            Beta.enableNotification = true;
-            //设置Wifi下自动下载
-            Beta.autoDownloadOnWifi = false;
-            //设置是否显示弹窗中的apk信息
-            Beta.canShowApkInfo = true;
-            //关闭热更新
-            Beta.enableHotfix = false;
-            strategy.setCrashHandleCallback(new CrashHandler());
-            // 统一初始化Bugly产品，包含Beta
-            Bugly.setIsDevelopmentDevice(getApplicationContext(), BuildConfig.DEBUG);
-            Bugly.init(getApplicationContext(), "ab6c4abe4f", BuildConfig.DEBUG, strategy);
-            Beta.init(this.getApplicationContext(), BuildConfig.DEBUG);
-//            Beta.upgradeListener = new UpgradeListener() {
-//                @Override
-//                public void onUpgrade(int i, UpgradeInfo upgradeInfo, boolean isManual, boolean isSilence) {
-//                    try {
-//                        LogUtils.loge("Beta.upgrade onUpgrade i = " + i + ",upgradeInfo = " + upgradeInfo + ", isManual = " + isManual + ",isSilence = " + isSilence);
-//                    } catch (Throwable throwable) {
-//                        throwable.printStackTrace();
+                /**
+                 * 点击过确认的弹窗在APP下次启动自动检查更新时会再次显示;
+                 */
+                Beta.showInterruptedStrategy = true;
+                //
+                Beta.upgradeDialogLayoutId = R.layout.layout_upgrade_dialog;
+                /**
+                 * 只允许在MainActivity上显示更新弹窗，其他activity上不显示弹窗;
+                 * 不设置会默认所有activity都可以显示弹窗;
+                 */
+                Beta.canNotShowUpgradeActs.add(SplashActivity.class);
+                //设置是否显示消息通知
+                Beta.enableNotification = true;
+                //设置Wifi下自动下载
+                Beta.autoDownloadOnWifi = false;
+                //设置是否显示弹窗中的apk信息
+                Beta.canShowApkInfo = true;
+                //关闭热更新
+                Beta.enableHotfix = false;
+                strategy.setCrashHandleCallback(new CrashHandler());
+                // 统一初始化Bugly产品，包含Beta
+                Bugly.setIsDevelopmentDevice(getApplicationContext(), BuildConfig.DEBUG);
+                Bugly.init(getApplicationContext(), "ab6c4abe4f", BuildConfig.DEBUG, strategy);
+                Beta.init(getApplicationContext(), BuildConfig.DEBUG);
+//                Beta.upgradeListener = new UpgradeListener() {
+//                    @Override
+//                    public void onUpgrade(int i, UpgradeInfo upgradeInfo, boolean isManual, boolean isSilence) {
+//                        try {
+//                            LogUtils.loge("Beta.upgrade onUpgrade i = " + i + ",upgradeInfo = " + upgradeInfo + ", isManual = " + isManual + ",isSilence = " + isSilence);
+//                        } catch (Throwable throwable) {
+//                            throwable.printStackTrace();
+//                        }
 //                    }
-//                }
-//            };
-//            Beta.upgradeStateListener = new UpgradeStateListener() {
-//                @Override
-//                public void onUpgradeFailed(boolean isManual) {
-//                    try {
-//                        LogUtils.loge("Beta.upgrade onUpgradeFailed isManual = " + isManual);
-//                    } catch (Throwable throwable) {
-//                        throwable.printStackTrace();
+//                };
+//                Beta.upgradeStateListener = new UpgradeStateListener() {
+//                    @Override
+//                    public void onUpgradeFailed(boolean isManual) {
+//                        try {
+//                            LogUtils.loge("Beta.upgrade onUpgradeFailed isManual = " + isManual);
+//                        } catch (Throwable throwable) {
+//                            throwable.printStackTrace();
+//                        }
 //                    }
-//                }
 //
-//                @Override
-//                public void onUpgradeSuccess(boolean isManual) {
-//                    try {
-//                        LogUtils.loge("Beta.upgrade onUpgradeSuccess isManual = " + isManual);
-//                    } catch (Throwable throwable) {
-//                        throwable.printStackTrace();
+//                    @Override
+//                    public void onUpgradeSuccess(boolean isManual) {
+//                        try {
+//                            LogUtils.loge("Beta.upgrade onUpgradeSuccess isManual = " + isManual);
+//                        } catch (Throwable throwable) {
+//                            throwable.printStackTrace();
+//                        }
 //                    }
-//                }
 //
-//                @Override
-//                public void onUpgradeNoVersion(boolean isManual) {
-//                    try {
-//                        LogUtils.loge("Beta.upgrade onUpgradeNoVersion isManual = " + isManual);
-//                    } catch (Throwable throwable) {
-//                        throwable.printStackTrace();
+//                    @Override
+//                    public void onUpgradeNoVersion(boolean isManual) {
+//                        try {
+//                            LogUtils.loge("Beta.upgrade onUpgradeNoVersion isManual = " + isManual);
+//                        } catch (Throwable throwable) {
+//                            throwable.printStackTrace();
+//                        }
 //                    }
-//                }
 //
-//                @Override
-//                public void onUpgrading(boolean isManual) {
-//                    try {
-//                        LogUtils.loge("Beta.upgrade onUpgrading isManual = " + isManual);
-//                    } catch (Throwable throwable) {
-//                        throwable.printStackTrace();
+//                    @Override
+//                    public void onUpgrading(boolean isManual) {
+//                        try {
+//                            LogUtils.loge("Beta.upgrade onUpgrading isManual = " + isManual);
+//                        } catch (Throwable throwable) {
+//                            throwable.printStackTrace();
+//                        }
 //                    }
-//                }
 //
-//                @Override
-//                public void onDownloadCompleted(boolean isManual) {
-//                    try {
-//                        LogUtils.loge("Beta.upgrade onDownloadCompleted isManual = " + isManual);
-//                    } catch (Throwable throwable) {
-//                        throwable.printStackTrace();
+//                    @Override
+//                    public void onDownloadCompleted(boolean isManual) {
+//                        try {
+//                            LogUtils.loge("Beta.upgrade onDownloadCompleted isManual = " + isManual);
+//                        } catch (Throwable throwable) {
+//                            throwable.printStackTrace();
+//                        }
 //                    }
-//                }
-//            };
-        } catch (Exception e) {
-            e.printStackTrace();
+//                };
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+
     }
 
     private final class CrashHandler extends CrashReport.CrashHandleCallback {
